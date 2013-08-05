f85e657cf53cf75f9d1a9494c84a7547fe058ac2
gustavo_cm <gustavo_cm@10.1.49.219>
2013-08-05 16:53:34 -0300

    Adicionei o menu principal eo vetor de funcionarios.

--- Revisao/src/br/senai/sc/view/JanelaPrincipal.java
+++ Revisao/src/br/senai/sc/view/JanelaPrincipal.java
@@ -1,31 +1,47 @@
 package br.senai.sc.view;
 
+
+import br.senai.sc.model.negocio.Funcionario;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
  * Classe que controla toda a parte visual da aplicação
  *
  * @version 1.0 31/07/2013
- * @author bruna_zakrzeski
+ * @author gustavo_cm
  */
 public class JanelaPrincipal {
 
-    public void mostraMenu() {
+    public static void main(String[] args) {
         
-       int opcao;
+     List<Funcionario> funcionarios = new ArrayList<>();
+        mostraMenu(funcionarios);
+     }
+    
+    public static void mostraMenu(List<Funcionario>
+            funcionarios) {
+        JanelaFuncionario jf = new JanelaFuncionario();
+    
+        int opcao = 0;
         
 
         do {
-            opcao - Integer.parseInt
-                    JOptionPane.showInputDialog("Informe a opção:"
+            opcao = Integer.parseInt
+                    (JOptionPane.showInputDialog
+                    (null, "Informe a opção:"
                             + "\n1 - Funcionário"
                             + "\n2 - Cliente"
-                            + "\n3 - Fornecedor
+                            + "\n3 - Fornecedor"
                             + "\n4 - Transportadora"
+                            + "\n5 - Sair"));
        
         
         switch (opcao) {
             case 1:
+                jf.menuFuncionario(funcionarios);
+                
                 break;
             case 2:
                 break;
@@ -33,9 +49,21 @@ public class JanelaPrincipal {
                 break;
             case 4:
                 break;
-
+            case 5:
+                break;
             default:
-                JOptionPane.showMessageDialog(null, "Opção inexistente! ");
+                break;
+        }    
+             
+        }while (opcao !=0);
+        
         }
-    }
-}
+}      
+        
+
+
+        
+        
+    
+    
+
