8512806acd746e5ec2c0d81aeceaba105cdc951d
gustavo_cm <gustavo_cm@10.1.49.219>
2013-08-05 16:52:23 -0300

    Adicionei os metodos listar e salvar.

--- Revisao/src/br/senai/sc/model/percistencia/FuncionarioDAO.java
+++ Revisao/src/br/senai/sc/model/percistencia/FuncionarioDAO.java
@@ -9,7 +9,7 @@ import javax.swing.JOptionPane;
 /** 
  * Classe que guarda as informações aos funcionário
  * @version 1.0   31/07/2013
- * @author bruna_zakrzeski
+ * @author gustavo_cm
  */
 
 public class FuncionarioDAO {
@@ -17,19 +17,44 @@ public class FuncionarioDAO {
     
     //Vetor dinâmico para armazenar funcionários cadastros.
     
-    private List<Funcionario> funcionarios = new ArrayList<>();
+    
     
     
     
     //Método para adicionar um funcionário no vetor de funcionários.
     
-    public void adicionar(Funcionario f){
-        funcionarios.add(f);
+    public void adicionar(Funcionario f,
+            List<Funcionario> funcionario){
+        funcionario.add(f);
  
-JOptionPane.showMessageDialog(null, "Funcionário" + "Adicionado Com Sucesso!");
+JOptionPane.showMessageDialog(null, 
+        "Funcionário" + " Adicionado Com Sucesso!");
    
     
     
     
+    }
+    
+    /*
+     * método que lista todos os funcionarios
+     */
+    public void listar(List<Funcionario> funcionarios){
+        String msg = " ";
+        for (Funcionario f : funcionarios) {
+            msg = msg + "\nNome: " +f.getNome()
+                    + "\nData de nascimento: " + f.getDataNascimento()
+                    + "\nRg: " + f.getRg()
+                    + "\nCpf " + f.getCpf()
+                    + "\nEndereço: " + f.getEndereco()
+                    + "\nTelefone: " + f.getTelefone()
+                    + "\nData Cadastro: " + f.getDataCadastro()
+                    + "\nSalário: " + f.getSalario()
+                    + "\nData admissao: " + f.getDataAdmissao()
+                    + "\nCtps: " + f.getCtps()
+                    + "\nCargo: " + f.getCargo();
+            
+        }
+        JOptionPane.showMessageDialog(null, msg);
+        
     }
 }
